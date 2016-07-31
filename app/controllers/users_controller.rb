a44de072a3c150eb1beec69c0d51e817a520d608
dninja777 <duroneppsjr@hotmail.com>
2016-07-31 09:07:18 -0400

    Sign up user from ui

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,26 @@ class UsersController < ApplicationController
 
 	end
 
+	def create
+		debugger
+		@user = User.new(user_params)
+		if @user.save
+			flash[:success] = "Welcome to alpha blog #{@user.username}"
+			redirect_to articles_path
+
+		else
+
+			render 'new'
+		end
+
+	end
+
+	private
+
+	def user_params
+
+		params.require(:user).permit(:username, :email, :password)
+
+	end
+
 end
